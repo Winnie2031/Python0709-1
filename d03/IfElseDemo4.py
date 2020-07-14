92d1b8f0a85fec985e1fe7ed47c8f148ed1ba300
vincenttuan <vincenttuan@gmail.com>
2020-07-14 21:05:59 +0800

    Python

--- d03/IfElseDemo4.py
+++ d03/IfElseDemo4.py
@@ -9,3 +9,11 @@ print(score >= 60 and score % 2 == 1)
 print(score >= 60 or score % 2 == 1)
 print(score >= 60 or score % 2 == 1 and score % 2 == 0)
 print(score >= 60 and score % 2 == 1 or score % 2 == 0)
+
+x = True
+y = False
+z = False
+if y and x or x:
+    print("Yes")
+else:
+    print("no")
