85d1e4abd22bdc7d77e09d3fb472ea1a41aa3b75
vincenttuan <vincenttuan@gmail.com>
2020-07-16 21:01:30 +0800

    Python

--- d04/GuessNumber.py
+++ d04/GuessNumber.py
@@ -1,7 +1,13 @@
-ans = 43
+import random
+ans = random.randint(1, 99)
 min, max = 0, 100
 while True:
     guess = int(input('請在%d~%d之間猜一數字:' % (min, max)))
+    # 驗證範圍 ?
+    if guess <= min or guess >= max:
+        print('輸入範圍錯誤, 請重新輸入')
+        continue
+    # 是否猜對 ?
     if guess > ans:
         max = guess
     elif guess < ans:
