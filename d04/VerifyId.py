13e767ade86b688e0f8fedf07f1b9485e3b15cdc
vincenttuan <vincenttuan@gmail.com>
2020-07-16 19:09:17 +0800

    Python

--- d04/VerifyId.py
+++ d04/VerifyId.py
@@ -5,7 +5,6 @@ delta = '19876543211'
 print(id)
 print(id2)
 print(delta)
-
 sum = int(id2[0]) * int(delta[0]) + \
       int(id2[1]) * int(delta[1]) + \
       int(id2[2]) * int(delta[2]) + \
@@ -17,4 +16,6 @@ sum = int(id2[0]) * int(delta[0]) + \
       int(id2[8]) * int(delta[8]) + \
       int(id2[9]) * int(delta[9]) + \
       int(id2[10]) * int(delta[10])
+
 print(sum)
+print("正確" if sum % 10 == 0 else "錯誤")
