33cbb451e645a851e8ce11dd9ea3eee5a24bfa38
vincenttuan <vincenttuan@gmail.com>
2020-07-21 19:19:40 +0800

    Python

--- d05/PokerGame.py
+++ d05/PokerGame.py
@@ -10,8 +10,20 @@ def getScore(p):
 poker = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'] * 4
 random.shuffle(poker)  # 洗牌
 print(poker)
+
+# 牌局開始先發二張
 p1 = poker.pop()
 p2 = poker.pop()
 sum = getScore(p1) + getScore(p2)
 print('已拿:', p1, p2, '總分:', sum)
 print('剩餘:', poker)
+
+# 繼續拿牌 ?
+while True:
+    ask = input('是否要拿牌(y/n)? ')
+    if ask == 'y':
+        p = poker.pop()
+        sum += getScore(p)
+        print('再拿:' + p, '總分:', sum)
+    else:
+        break
