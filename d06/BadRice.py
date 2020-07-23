f3c46d47e2ddf1c9a7ecd72227fc8eaede6a671f
vincenttuan <vincenttuan@gmail.com>
2020-07-23 21:32:42 +0800

    Python

---
+++ d06/BadRice.py
@@ -0,0 +1,8 @@
+import requests
+import json
+path = 'https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx'
+data = requests.get(path).text  # 取得網路原始字串資料
+list = json.loads(data)  # 將 json 字串轉成 python 可用的物件
+
+for rice in list:
+    print(rice.get('品名'), rice.get('不合格原因'))
