cca4cb29d64692812ac438e2c52d459d920e7d85
vincenttuan <vincenttuan@gmail.com>
2020-07-23 21:47:58 +0800

    Python

--- d06/BadRice.py
+++ d06/BadRice.py
@@ -5,4 +5,6 @@ data = requests.get(path).text  # 取得網路原始字串資料
 list = json.loads(data)  # 將 json 字串轉成 python 可用的物件
 
 for rice in list:
-    print(rice.get('品名'), rice.get('不合格原因'))
+    name = str(rice.get('品名'))
+    if name.__contains__('台稉'):
+        print(name, rice.get('不合格原因'))
